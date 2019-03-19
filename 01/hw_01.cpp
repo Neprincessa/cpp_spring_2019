1be72f01861635a39d6a25afc8c38f47d8749d96
Anastasia Khudoyarova <asya-iriska@yandex.ru>
2019-03-19 15:08:27 +0300

    corrected
    Corrected search of indexes, corrected checking of prime numbers, deleted repeatable part of code in main, corrected amount of prime (parameters are indexes and don't count call prime function for similar numbers)

--- 01/hw_01.cpp
+++ 01/hw_01.cpp
@@ -5,53 +5,79 @@
 inline bool Prime(const int &number) {
 	if (number < 2)
 		return false;
-	for (int i = 2; i <= sqrt(number); i++)
+
+	int root = sqrt(number);
+
+	for (int i = 2; i <= root; i++)
 		if (number%i == 0)
 			return false;
 	return true;
 }
 
-int IndexRight(const int &number) {
-	int result, indexOfElementLessThanElement;
-	int flag = false;
-	
-	for (int i =0; i<Size; i++) {
-
-		if (Data[i] == number) {
-			result = i;
-			flag = true;
-		}
-		else 
-			if (Data[i] < number)
-				indexOfElementLessThanElement = i;
+int SearchLeft(int &left){
+	int pos = 0;
+	while (pos < Size) {
+		if (Data[pos] == left)
+			return pos;
+		else
+			pos++;
 	}
+	return -1;
+}
 
-	if (flag)
-		return result;
-	else
-		return indexOfElementLessThanElement;
+int SearchRight(int &right){
+	int pos = Size - 1;
+	while (pos >= 0) {
+		if (Data[pos] == right)
+			return pos;
+		else
+			pos--;
+	}
+	return -1;
 }
 
 
 
+//int IndexRight(const int &number) {
+//	int result, indexOfElementLessThanElement;
+//	int flag = false;
+//	for (int i =0; i<Size; i++) {
+//		if (Data[i] == number) {
+//			result = i;
+//			flag = true;
+//		}
+//		else
+//			if (Data[i] < number)
+//				indexOfElementLessThanElement = i;
+//	}
+//	if (flag)
+//		return result;
+//	else
+//		return indexOfElementLessThanElement;
+//}
+//bool ExistanceLeft(int &number) {
+//	bool exist = false;
+//	for (int i =0; i<Size; i++) {
+//		if (Data[i] == number)
+//			exist = true;
+//	}
+//	return exist;
+//}
 
-bool ExistanceLeft(int &number) {
-	bool exist = false;	
 
-	for (int i =0; i<Size; i++) {
-
-		if (Data[i] == number)
-			exist = true;
-	}
-	return exist;
-}
-
-int AmountOfPrimes(int &left, int &right, int &index) {
+int AmountOfPrimes(int left, int right) {
 	int res = 0;
-	for (int i = 0; i<=index;i++) {
-		if ((Data[i] >= left))
-			if (Prime(Data[i]))
-				res++;	
+		for (int i = left; i <= right; i++) {
+		if(Prime(Data[i])) {
+			res++;
+			while(i < right-1 && Data[i+1] == Data[i]){ //don't count similar numbers
+				i++;
+				res++;
+			}
+		}
+		else
+			while(i < right-1 && Data[i+1] == Data[i]) //don't count similar numbers
+				i++;
 	}
 	return res;
 }
@@ -62,37 +88,25 @@ int main(int argc, char* argv[]) {
 	if (argc < 3 || argc %2 == 0) 
 		return -1;
 
-	int left, right;
+	int left, right, l,r;
 	int resultAmountPrimes = 0;
 	int index;
-	if (argc == 3) {
-		left = std::atoi(argv[1]);
-		right = std::atoi(argv[2]);
 
-		index = IndexRight(right);
+	for (int i = 1; i < argc; i++) {
+
+		left = std::atoi(argv[i]);
+		right = std::atoi(argv[i+1]);
 		
+		l = SearchLeft(left);
+		r = SearchRight(right);
+			
 		if (!ExistanceLeft(left))
 			std::cout<<"0";
 		else 
-			resultAmountPrimes = AmountOfPrimes(left, right,index);
-		std::cout<<resultAmountPrimes;
-	}
-	else {
-		for (int i = 1; i<=argc-2; i++) {
-
-			left = std::atoi(argv[i]);
-			right = std::atoi(argv[i+1]);
-		
-			index = IndexRight(right);
-			
-			if (!ExistanceLeft(left))
-				std::cout<<"0";
-			else 
-				resultAmountPrimes = AmountOfPrimes(left, right,index);
-			std::cout<<resultAmountPrimes<<"\n";
+			resultAmountPrimes = AmountOfPrimes(l, r);
+			std::cout<<resultAmountPrimes<<std::endl;
 			i++;
 
-		}
 	}
 
 
