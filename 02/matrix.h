a276b38d794ca3479b9e22b86687bb9246c99f1e
Neprincessa <asya-iriska@yandex.ru>
2019-03-26 19:19:10 +0300

    Correcrted HW

--- 02/matrix.h
+++ 02/matrix.h
@@ -5,20 +5,25 @@ class Matrix
 private:
 	class Row
 	{
-	public:
-		std::vector<int> v;
-		Row() {};
-		Row (int size) {
-			v.resize(size);
+	private:
+		size_t size;
+		std::vector<int> v;	
+	public:		
+		Row() 
+		{
 		}
-		
-		int& operator[](int i) {
+		Row (size_t size): size(size), v(std::vector<int>(size))
+		{
+		}
+		int& operator[](int i) 
+		{
 			if (i < 0 || i >= v.size())
 				throw std::out_of_range("");
 			else
 				return v[i];
 		}
-		const int& operator[](int i)  const {
+		const int& operator[](int i)  const 
+		{
 			if (i < 0 || i >= v.size())
 				throw std::out_of_range("");
 			else
@@ -26,30 +31,35 @@ private:
 		}
 	};
 	std::vector <Row> m_rows;
-	int rows, columns;
+	size_t rows, columns;
 public:
 	
-	Matrix(int rows_m, int columns_n) : rows(rows_m), columns(columns_n){
+	Matrix(size_t rows_m, size_t columns_n) : rows(rows_m), columns(columns_n)
+	{
 		m_rows = std::vector<Row>(rows);
 		for (int i = 0; i < rows; i++)
 			m_rows[i] = Row(columns);
 	}
 	
-	int getRows() const{
+	int getRows() const
+	{
 		return rows;
 	}
 	
-	int getColumns() const{
+	int getColumns() const
+	{
 		return columns;
 	}
 	
-	Row& operator[](int i ) {
+	Row& operator[](int i ) 
+	{
 		if (i >= rows || i < 0)
 			throw std::out_of_range("");
 		else
 			return m_rows[i];
 	}
-	const Row& operator[](int i ) const {
+	const Row& operator[](int i ) const 
+	{
 		if (i >= rows || i < 0)
 			throw std::out_of_range("");
 		else
@@ -57,7 +67,8 @@ public:
 	}
 	
 	
-	const bool operator==(const Matrix& other) const{
+	bool operator==(const Matrix& other) const
+	{
 		int flag = 0;
 		if (rows == other.getRows() && columns == other.getColumns()) {
 			for (int i = 0; i < rows; i++) {
@@ -66,13 +77,12 @@ public:
 						flag++;
 			}
 		}
-		if (flag == (rows * columns))
-			return true;
-		else
-			return false;
+		
+		return (flag == (rows * columns));
 	}
 	
-	Matrix& operator*=(int number) {
+	Matrix& operator*=(int number) 
+	{
 		for (int i = 0; i < rows; i++)
 			for (int j = 0; j < columns; j++)
 				m_rows[i][j] *= number;
@@ -84,3 +94,4 @@ public:
 	}
 	
 };
+
